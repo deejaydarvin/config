12bfc509637055ae7ec834098d554397e10963d3
Robert K <rkuennem@mpi-inf.mpg.de>
2014-04-11 11:52:02 +0200

    bootstrap.sh updated

--- bootstap.sh
+++ bootstap.sh
@@ -8,3 +8,5 @@ homeshick clone deejaydarvin/config
 # homeshick clone git@github.com:deejaydarvin/config.git
 
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.homesick/repos/zsh-syntax-highlighting
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
